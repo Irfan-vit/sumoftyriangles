41ed78112ea7fe2db26832c1478fe46376b8a86d
Irfan-vit <65129024+Irfan-vit@users.noreply.github.com>
2021-06-13 10:06:00 -0700

    added area and fixed errors in previous code

--- src/App.js
+++ src/App.js
@@ -2,36 +2,63 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  var [input, setInput] = useState([]);
-  var [output, setOutput] = useState("enter the angles");
-  //hypotenuse usestates
+  // Find hypotenuse usestates and functions
   var [outputH, setOutputH] = useState("");
   var [side1, setSide1] = useState("");
   var [side2, setSide2] = useState("");
   function ClickHandlerH() {
     outputH = Math.sqrt(side1 ** 2 + side2 ** 2);
     setOutputH(outputH);
+    setSide1(0);
+    setSide2(0);
   }
-  //hypotenuse end
+  // Hypotenuse end
+
+  // Find area of triangle usestates and functions
+  var [base, setBase] = useState("");
+  var [height, setHeight] = useState("");
+  var [area, setArea] = useState("");
+  function clickHandler() {
+    var ans = (1 / 2) * base * height;
+    setArea(ans);
+    setBase(0);
+    setHeight(0);
+  }
+  // Area End
+
+  // Is triangle Valid usestates and functions
+  var [input, setInput] = useState([]);
+  var [output, setOutput] = useState("enter the angles");
+
   function angle1InputHandler(event) {
     var input1 = event.target.value;
     setInput((input) => [...input, input1]);
     input.splice(0, 1);
   }
   console.log(input);
+
   function angle2InputHandler(event) {
     var input2 = event.target.value;
     setInput((input) => [...input, input2]);
     input.splice(1, 1);
   }
+
   function angle3InputHandler(event) {
     var input3 = event.target.value;
     setInput((input) => [...input, input3]);
     input.splice(2, 1);
   }
+
   function ClickHandler() {
     input = input.map(Number);
-    var sum = input.reduce((result, number) => result + number);
+    if (input === []) {
+      alert("I am an alert box!");
+    }
+    try {
+      var sum = input.reduce((result, number) => result + number);
+    } catch (err) {
+      alert("Please Enter the angles => " + err);
+    }
     setInput((input) => [0, 0, 0]);
     console.log(sum);
     if (sum === 180) {
@@ -42,27 +69,38 @@ export default function App() {
       setOutput(output);
     }
   }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <input type="number" onChange={angle1InputHandler} value={input[0]} />
-      <input type="number" onChange={angle2InputHandler} value={input[1]} />
-      <input type="number" onChange={angle3InputHandler} value={input[2]} />
-      <button
-        type="reset"
-        onClick={() => {
-          ClickHandler();
-        }}
-      >
-        calculate
-      </button>
-      <h1>{output}</h1>
+      <div id="check-traiangle">
+        <h1> Find whether given angles can form a triangle</h1>
+        <input type="number" onChange={angle1InputHandler} value={input[0]} />
+        <input type="number" onChange={angle2InputHandler} value={input[1]} />
+        <input type="number" onChange={angle3InputHandler} value={input[2]} />
+        <button
+          type="reset"
+          onClick={() => {
+            ClickHandler();
+          }}
+        >
+          calculate
+        </button>
+        <h3>{output}</h3>
+      </div>
+
       {/* hypotenuse */}
-      <div>
-        <h1>Enter the lengths</h1>
-        <input type="number" onChange={(e) => setSide1(e.target.value)} />
-        <input type="number" onChange={(e) => setSide2(e.target.value)} />
+      <div id="find-hypotenuse">
+        <h1>Enter the values to find hypotenuse</h1>
+        <input
+          type="number"
+          onChange={(e) => setSide1(e.target.value)}
+          value={side1}
+        />
+        <input
+          type="number"
+          onChange={(e) => setSide2(e.target.value)}
+          value={side2}
+        />
         <button
           onClick={() => {
             ClickHandlerH();
@@ -70,8 +108,24 @@ export default function App() {
         >
           calculate
         </button>
-        <h1>Hypotenuse is :</h1>
-        <h1>{outputH}</h1>
+        <h3>Hypotenuse is :{outputH}</h3>
+      </div>
+
+      {/* area */}
+      <div id="find-area">
+        <h1>Enter the base and height</h1>
+        <input
+          type="number"
+          onChange={(e) => setBase(e.target.value)}
+          value={base}
+        />
+        <input
+          type="number"
+          onChange={(e) => setHeight(e.target.value)}
+          value={height}
+        />
+        <button onClick={clickHandler}>Calculate</button>
+        <h3>Area of the triangle is : {area}</h3>
       </div>
     </div>
   );
